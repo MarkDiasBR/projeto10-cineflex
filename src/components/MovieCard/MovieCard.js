d020ebfb5b0aad95287db924befc028c59a2c2a1
MarkDiasBR <117422129+MarkDiasBR@users.noreply.github.com>
2023-03-17 03:36:08 -0300

    Requisito Tela Inicial

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,9 +1,11 @@
 import { MovieContainer } from "./styled"
 
-export default function MovieCard() {
+export default function MovieCard({ movie }) {
+    const { posterURL, title } = movie;
+
     return (
         <MovieContainer>
-            <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
+            <img src={posterURL} alt={title}/>
         </MovieContainer>
     )
 }
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,18 +1,33 @@
 import { PageContainer, ListContainer} from "./styled"
 import MovieCard from "../../components/MovieCard/MovieCard"
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import { BASE_URL } from "../../constants/urls"
+import axios from "axios"
 
 export default function HomePage() {
+    const [movies, setMovies] = useState(undefined)
+
+    useEffect(() => {    
+        axios.get(`${BASE_URL}/movies`)
+            .then(response => setMovies(response.data))
+            .catch(error => console.log(error.response.data))
+    }, [])
+    
+    if (movies === undefined) {
+        return <PageContainer>Carregando...</PageContainer>
+    }
+
     return (
         <PageContainer>
             Selecione o filme
 
             <ListContainer>
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
+                {movies.map(elem=>(
+                    <Link to={`/sessoes/${elem.id}`} key={elem.id}>
+                        <MovieCard movie={elem} />
+                    </Link>
+                ))}
             </ListContainer>
 
         </PageContainer>
